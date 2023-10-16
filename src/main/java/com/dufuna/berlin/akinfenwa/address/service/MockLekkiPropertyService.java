b0f9d2b74adde2297f8511d8af2e84e883b5560b
chris <akinfenwataiwo2019@gmail.com>
2023-10-16 03:53:15 +0100

    javaComment_2

--- src/main/java/com/dufuna/berlin/akinfenwa/address/service/MockLekkiPropertyService.java
+++ src/main/java/com/dufuna/berlin/akinfenwa/address/service/MockLekkiPropertyService.java
@@ -6,22 +6,36 @@ import java.util.List;
 
 public class MockLekkiPropertyService implements LekkiPropertyService{
 
+    /**
+     * @param lekkiProperty
+     * @return lekkiProperty
+     */
     @Override
     public LekkiProperty saveProperty(LekkiProperty lekkiProperty) {
 
         return lekkiProperty;
     }
 
+    /**
+     * @param id
+     * @return null
+     */
     @Override
     public LekkiProperty getProperty(int id) {
         return null;
     }
 
+    /**
+     * @return null
+     */
     @Override
     public List<LekkiProperty> getProperties() {
         return null;
     }
 
+    /**
+     * @return null
+     */
     @Override
     public LekkiProperty getProperty() {
         return null;
